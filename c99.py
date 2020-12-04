cfee628a6f38ea4e4142577dd0d474efed0be171
sparsh1801 <53870983+sparsh1801@users.noreply.github.com>
2020-12-04 18:59:43 +0530

    Add files via upload

---
+++ c99.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+path=input("entre the directory you want to sort:-")
+listoffiles=os.listdir(path)
+for file in listoffiles:
+    name,ext=os.path.splitext(file)
+    ext=ext[1:]
+    if ext=="":
+        continue
+    if os.path.exists(path+"/"+ext):
+        shutil.move(path+"/"+file,path+"/"+ext+"/"+file)
+    else:
+        os.makedirs(path+"/"+ext)
+        shutil.move(path+"/"+file,path+"/"+ext+"/"+file)
+
